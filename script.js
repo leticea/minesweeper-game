d792f12728236a1c5fdd6901c123bfd123607366
leticea <nascimento.leticeah@gmail.com>
2023-05-01 09:49:16 -0300

    feat(script.js): generateGrid function

---
+++ script.js
@@ -0,0 +1,21 @@
+const grid = document.getElementById("grid");
+let lockGame = false;
+const testMode = false;
+generateGrid();
+
+// Generate 10 * 10 Grid
+function generateGrid() {
+  lockGame = false;
+  grid.innerHTML = "";
+  for (var i = 0; i < 10; i++) {
+    row = grid.insertRow(i);
+    for (var j = 0; j < 10; j++) {
+      cell = row.insertCell(j);
+      cell.onclick = () => {
+        init(this);
+      };
+      let mine = document.createAttribute("mine");
+      mine.value = false;
+    }
+  }
+}
